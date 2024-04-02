6c5e2be9866502509fec3c63d0da2158bc0fc27a
ZiyeW-zz <125829074+ZiyeW-zz@users.noreply.github.com>
2024-04-01 21:36:48 -0500

    Init
    Xcode

---
+++ WildHacks2024/WildHacks2024/WildHacks2024App.swift
@@ -0,0 +1,17 @@
+//
+//  WildHacks2024App.swift
+//  WildHacks2024
+//
+//  Created by Ziye Wang on 4/1/24.
+//
+
+import SwiftUI
+
+@main
+struct WildHacks2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
